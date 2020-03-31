4093be8163047167a72d84e641594e6e6afa9277
Rupesh Chandra Mohanty <43889891+rupeshmohanty@users.noreply.github.com>
2020-03-31 12:23:35 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-$con=mysqli_connect("http://3.6.182.76/phpmyadmin/","admin_spectrum","spectrum","admin_spectrumDB") or die(mysqli_error($con));
+$con=mysqli_connect("3.6.182.76","admin_spectrum","spectrum","admin_spectrumDB") or die(mysqli_error($con));
 ?>
