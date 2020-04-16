47e09424c6d5bd25493dd16c47ea6f42e3edca0a
Rupesh Chandra Mohanty <43889891+rupeshmohanty@users.noreply.github.com>
2020-04-16 08:18:17 +0530

    Add files via upload

---
+++ details.php
@@ -0,0 +1,114 @@
+<?php 
+	setcookie("PHPSESSID","",time()-1);
+ ?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Spectrum Internship Drive 2020</title>
+		<link rel="icon" href="images/sitelogo.jpg">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/social_media.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+	</head>
+	<body class="subpage">
+
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<nav id="nav">
+						<a href="index.php">Home</a>
+						<a href="contact_us.html">Contact Us</a>
+					</nav>
+					<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+				</div>
+			</header><br/>
+			<form role="form" method="post" action="form.php" enctype="multipart/form-data"><br>
+<button type="submit" class="btn btn-primary" style="float:right;margin-right: 20px;" >Login</button>
+<button type="submit" class="btn btn-primary" style="float:right;margin-right: 5px;">Registration Page</button>
+</form>
+
+<img style="position: relative;width:300px;margin-left: 520px;" class="logo" src="images/spectrumlogo.png">
+<style>
+	@media only screen and (max-width: 600px) {
+ .logo{
+ 	width:0px;
+ 	height: 0px;
+ }
+}
+</style>
+<!--================= Details Section ==================-->
+<h2>Spectrum Internship Drive</h2><br/>
+<div style="text-align: center;font-size: 18px;"> 
+Spectrum club is proud to announce Spectrum Internship Drive 2020 for all 1st, 2nd and 3rd years students of College of Engineering and Technology Bhubaneswar.</div><br/>
+<div style="text-align: center;font-size: 18px;">As per AICTE, due to the current pandemic caused by Coronavirus, students are directed not to take any internships outside the organization during summer, but instead take up some challenging projects/problems which can be worked upon by their homes. In view of this, we have decided to provide you this opportunity to showcase your skills and work on challenging problems through our internship drive.</div><br/>
+<div style="text-align: center;font-size: 18px;">Register now on the website with all the details. </div><br/>
+<div style="text-align: center;font-size: 18px;">The internship is divided into three phases. 
+Each phase will consist of an assignment/task which has to be completed before the end of the corresponding phase of the internship.
+At the end of the internship certificates will be provided based on your aggregate performance in all the 3 rounds.
+Internship has 8 themes.
+No prerequisite knowledge is required to take part in the internship.
+The tasks are designed in a manner that task 1 as beginner , task 2 as intermediate and  task 3 as advance.</div><br/>
+<div style="font-size: 18px;margin-left: 10px;"> 
+The various tracks of the internship include:-<br/><br/>
+<ul> 
+<li>Web Development</li>
+<li>Python development</li> 
+<li>Android App Development</li>  
+<li>MATLAB</li>
+<li>Image Processing</li>  
+<li>Embedded Systems</li>  
+<li>Graphic Designing</li>  
+<li>Data Science / ML</li>
+</ul>
+</div><br/>
+<div style="text-align: center;font-size: 18px;">
+<b>PROCEDURE:- <br>
+Visit www.spectrumcet.com .<br>
+Register at spectrum internship drive.<br>
+Last date of registration is 20/04/2020.<br>
+On 21/04/2020 all registered students will get login id and password.<br>
+Login in the site using id and password. <br>
+Get to know the theme assigned to you, start working on it, and submit the assignment before the deadline.<br></b>
+</div><br/>
+
+
+
+<form role="form" method="post" action="form.php" enctype="multipart/form-data">
+<button type="submit" class="btn btn-primary" style="float:right;">I have read all the details and want to continue to the registration page.</button><br/><br/><br/>
+</form>
+			<!--==================== Footer ========================-->
+			<footer id="footer">
+				<div class="inner">
+					<div class="flex">
+						<div class="copyright">
+							&copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.
+						</div>
+						<ul class="icons">
+							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon fa-linkedin"><span class="label">linkedIn</span></a></li>
+							<li><a href="#" class="icon fa-pinterest-p"><span class="label">Pinterest</span></a></li>
+							<li><a href="#" class="icon fa-vimeo"><span class="label">Vimeo</span></a></li>
+						</ul>
+					</div>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
